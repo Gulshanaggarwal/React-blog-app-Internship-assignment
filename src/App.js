e59801519b41a9f04c21460c07f1f24b5bacd9ca
Gulshan Aggarwal <gulshanaggarwal333@gmail.com>
2021-07-24 17:20:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Browserouter as Router,Switch,Route} from 'react-router-dom';
+
+function App() {
+  return (
+   <h1>Hello React</h1>
+  );
+}
+
+export default App;
